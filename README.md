4f7efe87dd2bcc6d95c3f27d1212a52bb9d2c1a0
d3lph1 <d3lph1.contact@gmail.com>
2023-03-24 13:55:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Moebious
+
+ML algorithms benchmarking and synthetic data generation framework
+
+<p align="center">
+<img src ="moebius.svg" width="300">
+</p>
+
+## Installation
+
+### PIP
+
+Work in progress...
+
+### Docker
+
+```bash
+git clone https://github.com/D3lph1/moebius.git moebius && cd moebius
+```
+
+```bash
+docker-compose up -d
+```
+
+## Usage
+
+See examples of usage in `tutorials` folder.
+
+## Attribution
+
+[Image](https://commons.wikimedia.org/wiki/File:Moebius_strip.svg) of Moebius strip by 	Krishnavedala /
+[CC BY](https://commons.wikimedia.org/wiki/User:Krishnavedala)
+
+## License
+
+This code is published under the [MIT license](https://opensource.org/licenses/MIT). This means you
+can do almost anything with it, as long as the copyright notice and the accompanying license file
+is left intact.
---
+++ moebius/data/generator.py
@@ -0,0 +1,292 @@
+from abc import ABC, abstractmethod
+from typing import TypeVar, Generic, Union, Optional, NewType, Callable
+from collections.abc import Iterable, Iterator
+from enum import Enum
+from moebius.models.gmm import olr
+
+T = TypeVar('T')
+
+"""
+Interface for all classes that supply data
+
+T - type of suppliable data 
+"""
+
+
+class DataGenerator(ABC, Generic[T], Iterator[T]):
+    @abstractmethod
+    def next(self) -> T:
+        pass
+
+    def __iter__(self):
+        return self
+
+    """
+    Default implementation of magic method supplies data infinitely. It is your
+    responsibility to interrupt (break) loop in order to prevent infinite iterations.
+    """
+
+    def __next__(self) -> T:
+        return self.next()
+
+
+class OptionallyFiniteDataGenerator(DataGenerator[T]):
+    def __init__(self):
+        self.__iter = 0
+        self.__max_iters = None
+
+        pass
+
+    def set_max_iters(self, max_iters: Optional[int]):
+        self.__max_iters = max_iters
+
+    def get_max_iters(self) -> Optional[int]:
+        return self.__max_iters
+
+    def is_iterations_restricted(self) -> bool:
+        return self.__max_iters != None
+
+    def reset_iters():
+        self.__iter = 0
+
+    def __next__(self) -> T:
+        if self.is_iterations_restricted():
+            if self.__iter >= self.get_max_iters():
+                raise StopIteration
+
+        val = super().__next__()
+        self.__iter += 1
+
+        return val
+
+
+class ConstDataGenerator(OptionallyFiniteDataGenerator[T]):
+    def __init__(self, const: T):
+        super().__init__()
+        self.const = const
+
+    def next(self) -> T:
+        return self.const
+
+
+class IterableDataGenerator(OptionallyFiniteDataGenerator[T]):
+    def __init__(self, iterable: Iterable[T]):
+        super().__init__()
+        self.iterator = iter(iterable)
+
+    def next(self) -> T:
+        return next(self.iterator)
+
+
+class GridRange(ABC, Generic[T]):
+    @abstractmethod
+    def get_value(self) -> T:
+        pass
+
+    @abstractmethod
+    def within(self) -> bool:
+        pass
+
+    @abstractmethod
+    def step(self) -> T:
+        pass
+
+    @abstractmethod
+    def reset(self):
+        pass
+
+
+Numeric = NewType('Numeric', Union[int, float])
+
+RangeDirection = Enum('RangeDirection', ['INCREMENTAL', 'DECREMENTAL'])
+
+
+class NumericGridRange(GridRange[Numeric]):
+    def __init__(self, a: Numeric, b: Numeric, step: Numeric):
+        if step <= 0:
+            raise ValueError("Step must be a positive number")
+
+        self.__a = a
+        self.__b = b
+        self.__step = step
+        self.__value = self.get_initial_value()
+
+        if a <= b:
+            self.__direction = RangeDirection.INCREMENTAL
+        else:
+            self.__direction = RangeDirection.DECREMENTAL
+
+    def unit(a: Numeric, b: Numeric):
+        return NumericGridRange(a, b, 1)
+
+    def const(const: Numeric):
+        return NumericGridRange.unit(const, const)
+
+    def get_value(self) -> Numeric:
+        return self.__value
+
+    def get_initial_value(self) -> Numeric:
+        return self.__a
+
+    def within(self) -> bool:
+        if self.__direction == RangeDirection.INCREMENTAL:
+            return self.__value >= self.__a and self.__value < self.__b
+        else:
+            return self.__value > self.__b and self.__value <= self.__a
+
+    def step(self) -> Numeric:
+        if self.__direction == RangeDirection.INCREMENTAL:
+            self.__value += self.__step
+
+            if self.__value > self.__b:
+                self.__value = self.__b
+        else:
+            self.__value -= self.__step
+
+            if self.__value < self.__b:
+                self.__value = self.__b
+
+        return self.__value
+
+    def reset(self):
+        self.__value = self.get_initial_value()
+
+    def get_direction(self) -> RangeDirection:
+        return self.__direction
+
+    def split(self, n: int):
+        if self.__a == self.__b:
+            return [NumericGridRange(self.__a, self.__b, self.__step)]
+
+        interval = [min(self.__a, self.__b), max(self.__a, self.__b)]
+
+        intervals = self.__split_interval(interval, n)
+
+        return [NumericGridRange(interval[0], interval[1], self.__step) for interval in intervals]
+
+    def __split_interval(self, l, n: int):
+        w = (l[1] - l[0]) // n
+
+        return [[l[0] + i * w, l[0] + (i + 1) * w] for i in range(n)]
+
+    def __str__(self) -> str:
+        return f'NumericGridRange({self.__a, self.__b, self.__step})'
+
+
+DimensionRange = NewType('DimensionRange', GridRange[T])
+DimensionRanges = NewType('DimensionRanges', list[GridRange[T]])
+
+
+class EnumerationDirection(ABC):
+    @abstractmethod
+    def direct(self, ranges: DimensionRanges):
+        pass
+
+
+class ForwardEnumerationDirection(EnumerationDirection):
+    def direct(self, ranges: DimensionRanges):
+        return ranges
+
+
+class ReversedEnumerationDirection(EnumerationDirection):
+    def direct(self, ranges: DimensionRanges):
+        return reversed(ranges)
+
+
+class GridIterator(GridRange):
+    def __init__(
+            self,
+            ranges: DimensionRanges,
+            direction: EnumerationDirection = ReversedEnumerationDirection()
+    ):
+        self.ranges = ranges
+        self.direction = direction
+        self.first = True
+
+    def of(ranges, direction: EnumerationDirection = ReversedEnumerationDirection()):
+        new = []
+
+        for r in ranges:
+            if isinstance(r, list):
+                new.append(GridIterator.of(r, direction))
+            else:
+                new.append(r)
+
+        return GridIterator(new, direction)
+
+    def within(self) -> bool:
+        return any([r.within() for r in self.ranges])
+
+    def step(self):
+        return self.do_next(self.ranges)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.first:
+            self.first = False
+
+            return self.get_value()
+
+        return self.step()
+
+    def do_next(self, rs):
+        for r in self.direction.direct(rs):
+
+            if r.within():
+                r.step()
+
+                return self.get_value()
+
+            r.reset()
+
+        raise StopIteration
+
+    def split(self, n: int) -> list:
+        new = []
+
+        for i, r in enumerate(self.ranges):
+            for j, s in enumerate(r.split(n)):
+
+                if len(new) < j + 1:
+                    new.append([])
+
+                new[j].append(s)
+
+        result = []
+
+        for n in new:
+            result.append(GridIterator.of(n))
+
+        return result
+
+    def reset(self):
+        for r in self.ranges:
+            r.reset()
+
+    def get_value(self) -> list:
+        return [r.get_value() for r in self.ranges]
+
+    def get_range(self, idx: int):
+        return self.ranges[idx]
+
+    def __str__(self) -> str:
+        return 'GridIterator(' + ', '.join([str(r) for r in self.ranges]) + ')'
+
+
+class GridIteratopDataGenerator(OptionallyFiniteDataGenerator[T]):
+    def __init__(self, iterator: GridIterator):
+        super().__init__()
+        self.iterator = iterator
+
+    def next(self):
+        return self.supply(self.iterator.__next__())
+
+    @abstractmethod
+    def supply(self, data: any) -> any:
+        pass
+
+class GaussuanMixtureDataGenerator(GridIteratopDataGenerator[T]):
+    def supply(self, data: any) -> any:
+        return (data, olr(data[0], data[1], data[2]))
